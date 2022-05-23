6f06a02ee3076217b1e32aadfb023aa16f2637a1
qsun <773618446@qq.com>
2022-05-23 20:08:28 +0800

    feat2

---
+++ src/main/java/cn/bugstack/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,20 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+
+@SuppressWarnings({"rawtypes"})
+public class BeanDefinition {
+
+    private Class beanClass;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+}
---
+++ src/main/java/cn/bugstack/springframework/beans/factory/config/SingletonBeanRegistry.java
@@ -0,0 +1,8 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+
+public interface SingletonBeanRegistry {
+
+    Object getSingleton(String beanName);
+
+}
