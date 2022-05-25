0d7cae016a929c963ae188c70ecfdb196cd67cae
qsun <773618446@qq.com>
2022-05-25 22:02:33 +0800

    feat:实现注册bean有参构造

--- src/test/java/cn/bugstack/springframework/test/ApiTest.java
+++ src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -13,17 +13,13 @@ public class ApiTest {
         // 1.初始化 BeanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        // 2.注册 bean
+        // 2. 注入bean
         BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        // 3.第一次获取 bean
-        UserService userService = (UserService) beanFactory.getBean("userService");
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService", "qs");
         userService.queryUserInfo();
-
-        // 4.第二次获取 bean from Singleton
-        UserService userService_singleton = (UserService) beanFactory.getSingleton("userService");
-        userService_singleton.queryUserInfo();
     }
 
 }
--- src/test/java/cn/bugstack/springframework/test/bean/UserService.java
+++ src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -3,8 +3,23 @@ package cn.bugstack.springframework.test.bean;
 
 public class UserService {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void queryUserInfo(){
         System.out.println("查询用户信息");
     }
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("").append(name);
+        return sb.toString();
+    }
 }
