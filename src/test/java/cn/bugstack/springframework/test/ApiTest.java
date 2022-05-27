f2fe20830c4d30e3b76adbb7c8f5a7beb2d0ddb1
qsun <773618446@qq.com>
2022-05-27 21:15:10 +0800

    feat:利用AOP联盟的API和aspectj实现AOP，不结合spring

--- src/test/java/cn/bugstack/springframework/test/ApiTest.java
+++ src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -1,5 +1,12 @@
 package cn.bugstack.springframework.test;
 
+import cn.bugstack.springframework.aop.AdvisedSupport;
+import cn.bugstack.springframework.aop.MethodMatcher;
+import cn.bugstack.springframework.aop.TargetSource;
+import cn.bugstack.springframework.aop.aspectj.AspectJExpressionPointcut;
+import cn.bugstack.springframework.aop.framework.Cglib2AopProxy;
+import cn.bugstack.springframework.aop.framework.JdkDynamicAopProxy;
+import cn.bugstack.springframework.aop.framework.ReflectiveMethodInvocation;
 import cn.bugstack.springframework.beans.PropertyValue;
 import cn.bugstack.springframework.beans.PropertyValues;
 import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
@@ -11,17 +18,23 @@ import cn.bugstack.springframework.cores.io.DefaultResourceLoader;
 import cn.bugstack.springframework.cores.io.Resource;
 import cn.bugstack.springframework.cores.io.ResourceLoader;
 import cn.bugstack.springframework.test.bean.IUserDao;
+import cn.bugstack.springframework.test.bean.IUserService;
 import cn.bugstack.springframework.test.bean.UserService;
+import cn.bugstack.springframework.test.bean.UserServiceInterceptor;
 import cn.bugstack.springframework.test.common.MyBeanFactoryPostProcessor;
 import cn.bugstack.springframework.test.common.MyBeanPostProcessor;
 import cn.bugstack.springframework.test.event.CustomEvent;
 import cn.hutool.core.io.IoUtil;
+import org.aopalliance.intercept.MethodInterceptor;
 import org.junit.Before;
 import org.junit.Test;
 import org.openjdk.jol.info.ClassLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 
 public class ApiTest {
@@ -29,6 +42,7 @@ public class ApiTest {
     public void test_prototype() {
         // 1.初始化 BeanFactory
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+
         applicationContext.registerShutdownHook();
 
         // 2. 获取Bean对象调用方法
@@ -59,5 +73,77 @@ public class ApiTest {
         applicationContext.publishEvent(new CustomEvent(applicationContext, 1019129009086763L, "成功了！"));
 
         applicationContext.registerShutdownHook();
+    }
+
+
+    @Test
+    public void test_aop() throws NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut("execution(* cn.bugstack.springframework.test.bean.UserService.*(..))");
+
+        Class<UserService> clazz = UserService.class;
+        Method method = clazz.getDeclaredMethod("queryUserInfo");
+
+        System.out.println(pointcut.matches(clazz));
+        System.out.println(pointcut.matches(method, clazz));
+    }
+    @Test
+    public void test_dynamic() {
+/*        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        // 目标对象
+        IUserService userService = applicationContext.getBean("userService", UserService.class);*/
+        IUserService userService = new UserService();
+
+        // 组装代理信息
+        AdvisedSupport advisedSupport = new AdvisedSupport();
+        advisedSupport.setTargetSource(new TargetSource(userService));
+        advisedSupport.setMethodInterceptor(new UserServiceInterceptor());
+        advisedSupport.setMethodMatcher(new AspectJExpressionPointcut("execution(* cn.bugstack.springframework.test.bean.IUserService.*(..))"));
+
+        // 代理对象(JdkDynamicAopProxy)
+        IUserService proxy_jdk = (IUserService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        // 测试调用
+        System.out.println("测试结果：" + proxy_jdk.queryUserInfo());
+
+        // 代理对象(Cglib2AopProxy)
+        IUserService proxy_cglib = (IUserService) new Cglib2AopProxy(advisedSupport).getProxy();
+        // 测试调用
+        System.out.println("测试结果：" + proxy_cglib.register("花花"));
+    }
+
+    @Test
+    public void test_proxy_method() {
+        // 目标对象(可以替换成任何的目标对象)
+        Object targetObj = new UserService();
+
+        // AOP 代理
+        IUserService proxy = (IUserService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), targetObj.getClass().getInterfaces(), new InvocationHandler() {
+            // 方法匹配器
+            MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* cn.bugstack.springframework.test.bean.IUserService.*(..))");
+
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (methodMatcher.matches(method, targetObj.getClass())) {
+                    // 方法拦截器
+                    MethodInterceptor methodInterceptor = invocation -> {
+                        long start = System.currentTimeMillis();
+                        try {
+                            return invocation.proceed();
+                        } finally {
+                            System.out.println("监控 - Begin By AOP");
+                            System.out.println("方法名称：" + invocation.getMethod().getName());
+                            System.out.println("方法耗时：" + (System.currentTimeMillis() - start) + "ms");
+                            System.out.println("监控 - End\r\n");
+                        }
+                    };
+                    // 反射调用
+                    return methodInterceptor.invoke(new ReflectiveMethodInvocation(targetObj, method, args));
+                }
+                return method.invoke(targetObj, args);
+            }
+        });
+
+        String result = proxy.queryUserInfo();
+        System.out.println("测试结果：" + result);
+
     }
 }
--- src/test/java/cn/bugstack/springframework/test/bean/UserService.java
+++ src/test/java/cn/bugstack/springframework/test/bean/UserService.java
@@ -1,19 +1,34 @@
 package cn.bugstack.springframework.test.bean;
 
+import java.util.Random;
+
 /**
  * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack)
  */
-public class UserService {
+public class UserService implements IUserService {
 
     private String uId;
     private String company;
     private String location;
     private IUserDao userDao;
 
+    /*    public String queryUserInfo() {
+            return userDao.queryUserName(uId) + "," + company + "," + location;
+        }*/
     public String queryUserInfo() {
-        return userDao.queryUserName(uId) + "," + company + "," + location;
+        try {
+            Thread.sleep(new Random(1).nextInt(100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "小傅哥，100001，深圳";
+    }
+
+    @Override
+    public String register(String userName) {
+        return "注册用户：" + userName + " success！";
     }
 
     public String getuId() {
